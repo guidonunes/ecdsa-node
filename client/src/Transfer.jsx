5fffed3604bbeed7bcdfd34cd9721fe2b2f08ff5
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-20 15:17:58 -0300

    imported modules

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from 'ethereum-cryptography/secp256k1';
+import { toHex, utf8ToBytes } from 'ethereum-cryptography/utils';
+import { keccak256 } from 'ethereum-cryptography/keccak';
 
-function Transfer({ address, setBalance }) {
+function Transfer({ address, setBalance, privateKey, setPrivateKey }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
@@ -9,6 +12,12 @@ function Transfer({ address, setBalance }) {
 
   async function transfer(evt) {
     evt.preventDefault();
+
+    if(!privateKey) {
+      alert("Please enter a private key");
+      return;
+    }
+
 
     try {
       const {
